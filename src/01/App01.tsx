8d7d3a27dba91b57cf36ca915c9e214c358a4089
Nicefort <Nicefort>
2025-02-05 20:59:10 +0100

    Premier commit

---
+++ src/01/App01.tsx
@@ -0,0 +1,16 @@
+//App01.tsx
+import logo from './charingan.jpeg'
+
+export default function App01(){
+    return (
+        <div className='nice'>
+             <div>
+                <img src={logo} alt="" width={(500)} />
+            </div>
+            <h2>sharigan a 3 tomoe du celebre clan <span>uchiha</span> </h2>
+        </div>
+
+       
+
+    )
+}
