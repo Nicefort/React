21e71a145a5a7ea4951c99028e386713bba0dc5e
Nicefort <nicefortketeguia@gmail.com>
2025-02-20 17:55:00 +0100

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
+import "./index.css"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 
-import App from './03/App.tsx'
+import App06 from './06/FORM01.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App  />
+    <App06 />
   </StrictMode>,
 )
